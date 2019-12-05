c4ccdf32c2a2eb893d870157405394290e430ab2
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-12-05 11:14:36 +0900

    CSS,JS,Helperファイルをrails gコマンドで自動生成しない設定を追記

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module Instaclone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
   end
 end
