cfbd6094d0eac35a13a9d963bbd358a355b0b1b0
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-12-05 11:28:13 +0900

    userのnameとemailにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :email, presence: true
+  validates :email, length: { maximum: 255 }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
