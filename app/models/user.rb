b0298180f1e9cd5938ae0a222d22dfca261da26d
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-12-05 20:49:53 +0900

    セキュアパスワードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   validates :email, length: { maximum: 255 }
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
