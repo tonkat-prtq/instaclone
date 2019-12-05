0242ca53be2461c21cdaf34ed1f7e457fb9fd973
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-12-05 11:20:24 +0900

    Userテーブルにname, email, profile, image. p_dカラム追加

--- db/migrate/20191205021557_create_users.rb
+++ db/migrate/20191205021557_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.string :email
+      t.text :profile
+      t.text :image
+      t.string :password_digest
       t.timestamps
     end
   end
