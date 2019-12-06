d3a8bd52c95893de2885541e5ab12232ea2ec879
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-12-06 11:11:58 +0900

    ユーザープロフィールの編集のためeditアクションとupdateアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user.image.cache! unless @user.image.blank? # 既に画像が存在する場合にキャッシュを作成する
+  end
+
+  def update
+    if params[:back]
+      render :edit
+    else
+      if @user.update(user_params)
+        redirect_to user_path(@user.id), notice: "プロフィールを編集しました" # showアクション発火させ詳細画面に飛ばす
+      else
+        render :edit
+      end
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
   end
 end
