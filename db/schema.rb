9fe1e5d254f26e924600787a1be802a6738da78e
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-12-09 22:28:23 +0900

    modelの作成、blogsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_022831) do
+ActiveRecord::Schema.define(version: 2019_12_09_132718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
