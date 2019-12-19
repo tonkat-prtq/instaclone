002d257fc515531d29741cd992403a1e932b4b21
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2019-12-19 11:42:13 +0900

    favoriteのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :blogs
+  resources :favorites, only: [:create, :destroy]
 end
