79d412c841dd571847f4015ac6fe70cc5fb9d90e
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-01-13 23:20:13 +0900

    blogのvalidation追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,4 +3,8 @@ class Blog < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   mount_uploader :image, ImageUploader
+  validates :title, presence: true
+  validates :title, length: { maximum: 50 }
+  validates :content, presence: true
+  validates :content, length: { maximum: 140 }
 end
