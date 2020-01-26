4070c7e2a9298a4aaf594d579d5da9135e1aee31
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-01-26 15:12:25 +0900

    user.emailにメールが送れるように

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,6 +2,6 @@ class PostMailer < ApplicationMailer
   def post_mail(contact)
     @contact = contact
 
-    mail to: "k2t.y4kn@gmail.com", subject: "投稿確認メール"
+    mail to: @contact, subject: "投稿確認メール"
   end
 end
